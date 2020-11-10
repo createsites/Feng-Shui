e605282c6ef4e272f7b7cb717624bf2225b2a8b4
Romanenko-Vasilii <romvas-1989@mail.ru>
2020-11-10 14:21:53 +0700

    make gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,31 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 
-
-
-gulp.task('browser-sync', function() {
+// Static server
+ function bs() {
+  serveSass(); 
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
+
+ function serveSass() {
+  return src("./sass/**/*.sass", "./sass/**/*.scss")
+      .pipe(sass())
+      .pipe(autoprefixer({
+        cascade: false
+      }))
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
