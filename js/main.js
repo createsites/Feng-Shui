77b2910714d11cf9ba0afd4e22d32e2473507c97
Romanenko-Vasilii <romvas-1989@mail.ru>
2021-01-31 22:38:35 +0700

    fixed - menu

--- js/main.js
+++ js/main.js
@@ -6,14 +6,24 @@ $(document).ready(function(){
       }
     });
   });
-  $('.info__item').on('click', function() {
-    $('.info').css('display', 'none');
-  });
+  // $('.info__item').on('click', function() {
+  //   $('.info').css('display', 'none');
+  // });
 
   $('a[href^="#"]').click(function(){ 
     if(document.getElementById($(this).attr('href').substr(1)) != null) { 
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 700); 
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 150 }, 700); 
     }     
     return false;
   });
+  
+  var fixd = $('.header__head');
+  $(window).scroll(function () {
+		if ($(this).scrollTop() > 280) {
+			fixd.addClass('fixd');
+		} else {
+			fixd.removeClass('fixd');
+		}
+	});
+  
 });
