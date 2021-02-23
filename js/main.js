d1adf68851dc8d0fd51273c14d41a76cea8bc0c6
a.novoselov <admin@createsites.net>
2021-02-23 09:58:32 +0300

    Adding to basket with cookies, counting total price in short basket, updated gitignore.

--- js/main.js
+++ js/main.js
@@ -54,34 +54,70 @@ $(document).ready(function(){
 		}
 	});
 
-  // добавление в корзину
-  $('.buy').click(function () {
-      // к родителю добавил .size_kind - это общий класс для small и big, чтобы проще было искать
-      var size = $(this).parent('.size_kind');
-      // собираем инфу о товаре
-      var name;
-      var sizeKind;
-      var price;
-      var weight;
-      // стандарт
-      if (size.hasClass('small')) {
-          sizeKind = 'стандарт';
-          price = size.find('.small__price').text();    // этого дублирования кода можно избежать, если добавить общий класс для цены и веса
-          weight = size.find('.small__gramm').text();   // но, как говорится, и так сойдет)
-      }
-      // большой
-      else if (size.hasClass('big')) {
-          sizeKind = 'большой';
-          price = size.find('.big__price').text();    // я про дублирование этого кода говорил
-          weight = size.find('.big__gramm').text();   //
-      }
-      // ищем название товара
-      name = $(size).parents('.wrap__product').find('.product__title').text();
+    const cookieName = 'basket';
+    // объект корзины
+    var basket = {};
 
-      // тупо добавляем html код с товаром в блок корзины
-      // var strForAdding = '<div>' + name + '<br>Цена: ' + price + '<br>Вес: ' + weight + '<br>Размер: ' + sizeKind + '</div>';
-      // $('#basket .products_list').append(strForAdding);
-      // return false;
-  });
+    // функция добавления в корзину
+    basket.add = function (product) {
+        // добавляем объект продукта в массив корзины
+        basket.products.push(product);
+        // делаем из объекта строку json, чтобы хранить ее в cookies
+        var newCookie = JSON.stringify(basket.products);
+        // добавляем (обновляем) корзину в cookies
+        $.cookie(cookieName, newCookie, {path: '/'});
+        // пересчитываем и показываем цену в корзине
+        basket.refreshPrice();
+    };
+
+    // считает сумму в корзине
+    basket.getPrice = function () {
+        var totalPrice = 0;
+        // цикл по продуктам в корзине
+        $.each(basket.products, function (key, product) {
+            // прибавляем цену к сумме
+            totalPrice += parseInt(product.price);
+        });
+        return totalPrice;
+    };
+
+    // отображение цены в корзине
+    basket.refreshPrice = function () {
+        var newPrice = basket.getPrice();
+        fixd__basket.find('.total__price_amount').text(newPrice);
+    };
+
+    // добавление в корзину
+    $('.buy').click(function () {
+        // к родителю добавил .size_kind - это общий класс для small и big, чтобы проще было искать
+        var size = $(this).parent('.size_kind');
+        // собираем инфу о товаре
+        var product = {}; // объект с данными о товаре
+
+        // стандарт
+        if (size.hasClass('small')) {
+            product.sizeKind = 'стандарт';
+            product.price = size.find('.small__price').text().trim();    // этого дублирования кода можно избежать, если добавить общий класс для цены и веса
+            product.weight = size.find('.small__gramm').text().trim();   // но, как говорится, и так сойдет)
+        }
+        // большой
+        else if (size.hasClass('big')) {
+            product.sizeKind = 'большой';
+            product.price = size.find('.big__price').text().trim();    // я про дублирование этого кода говорил
+            product.weight = size.find('.big__gramm').text().trim();   //
+        }
+        var wrapProduct = $(size).parents('.wrap__product');
+        // ищем название товара
+        product.name = wrapProduct.find('.product__title').text().trim();
+        // картинка
+        product.img = wrapProduct.find('.product__img').attr('src');
+        // добавляем в корзину
+        basket.add(product);
+    });
+
+    // массив с объектами продуктов (при первой загрузке страницы берется из cookies)
+    basket.products = ($.cookie(cookieName) != null) ? JSON.parse($.cookie(cookieName)) : [];
+    // пересчитываем и показываем цену в корзине
+    basket.refreshPrice();
   
 });
