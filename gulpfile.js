818ff66eb7b5d18597a65f2532bea693b3cca2a6
Romanenko-Vasilii <romvas-1989@mail.ru>
2021-03-06 03:26:13 +0700

    feat file.min

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
 
 
 // Static server
@@ -28,4 +31,55 @@ const autoprefixer = require('gulp-autoprefixer');
       .pipe(browserSync.stream());
 };
 
+ function buildCSS(done) {
+   src('css/**/**.css').pipe(cleanCSS({compatibility: 'ie8'}))
+   .pipe(dest('dist/css/'));
+   done();
+ }
+
+ function buildJS(done) {
+    src(['js/**.js', '!js/**.min.js']).pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(minify({ext:{
+        min:'.js'
+        }
+      }))
+      .pipe(dest('dist/js/'));
+    src('js/**.min.js').pipe(dest('dist/js/'));
+    done();
+ }
+
+ function html(done) {
+  src('**.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest('dist/'));
+  done();
+ }
+
+ function php(done) {
+  src('**.php')
+    .pipe(dest('dist/'));
+  src('phpmailer/**/**')
+    .pipe(dest('dist/phpmailer/'));
+  done();
+ }
+
+ function fonts(done) {
+  src('fonts/**/**')
+    
+    .pipe(dest('dist/fonts'));
+  done();
+ }
+
+ function image(done) {
+  src('img/**/**')
+    
+    .pipe(dest('dist/img'));
+  done();
+ }
+  
+
+
+
+
 exports.serve = bs;
+exports.build = series(buildCSS, buildJS, html, php, fonts, image);
